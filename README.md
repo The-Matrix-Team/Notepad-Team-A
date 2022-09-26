c5907151d568250fedabd59d39c46c37f4e8f83c
“Narendra” <nikhilbajwal16@gmail.com>
2022-09-26 11:17:40 +0530

    All features are Integrated and Project is working till now

---
+++ README.md
@@ -0,0 +1,29 @@
+#Code is going on
+
+Done by now:
+
+- Text Editor Done
+- Basis Editing Done
+- Save Button
+- Back Button
+- User can copy all text by click of a button
+- User can see the list of notes made by user
+- Design Integration
+
+To Do:
+
+- User can create a new note and can switch between notes
+
+- Live the project
+- Documentation
+
+Features:
+
+1. User can make a note
+2. User can make some text bold / underlined
+3. User can copy all text by click of a button
+4. User can see the list of notes made by user
+5. User can create a new note and can switch between notes
+
+Images till now:
+![editor](./assets/img/editor.png)
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,37 @@
-import allNotesAPI from "./allNotesAPI.js";
+const allNotes = document.querySelector(".allNotes");
 
-var button = document.querySelector(".add-box");
+function deleteNote(id) {
+  console.log("delete");
+  const notes = JSON.parse(localStorage.getItem("notesapp-notes"));
+  const newNotes = notes.filter((note) => note.id != id);
+  localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
+  location.reload();
+}
 
-button.addEventListener("click", () => {
-  console.log(allNotesAPI.getAllNotes());
-});
+function showNotes() {
+  const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
+
+  if (!notes) return;
+  notes.forEach((note) => {
+    console.log(note.content);
+
+    let currentNote = `
+    <div class="note-div">
+          <h3 class="ti">${note.title}</h3>
+          <p class="dsc">
+          ${note.content}
+          </p>
+          <span>${note.date}</span>
+          <button class="delete">
+            <img src="assets/img/delete-alt.svg" alt=""  onclick="deleteNote('${note.id}')"/>
+          </button>
+        </div>
+ 
+
+  `;
+
+    allNotes.insertAdjacentHTML("afterend", currentNote);
+  });
+}
+
+showNotes();
